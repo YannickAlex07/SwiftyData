7aae337977d40ceae21ed76ded8970e59ba80b14
Yannick Alexander <yannick.alexander07@gmail.com>
2020-05-28 15:47:35 +0200

    Added Bool Extension
    Bool now features an Init that allows it to be initialized from given Data.

--- Sources/SwiftyData/Extensions/Datatypes/Bool+Init.swift
+++ Sources/SwiftyData/Extensions/Datatypes/Bool+Init.swift
@@ -1,8 +1,11 @@
-//
-//  File.swift
-//  
-//
 //  Created by Yannick Alexander on 28.05.20.
-//
 
 import Foundation
+
+extension Bool {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Bool.self, data) else { return nil }
+        self = value
+    }
+}
