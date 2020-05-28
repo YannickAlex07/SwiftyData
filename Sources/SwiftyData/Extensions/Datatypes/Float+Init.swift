729c5c1a68033fbe9cd0c519793ea49572bd26c3
Yannick Alexander <yannick.alexander07@gmail.com>
2020-05-28 15:55:33 +0200

    Added Float Inits
    All Floats now implement an extra Init that allows them to be initialized from given Data.

--- Sources/SwiftyData/Extensions/Datatypes/Float+Init.swift
+++ Sources/SwiftyData/Extensions/Datatypes/Float+Init.swift
@@ -1,8 +1,19 @@
-//
-//  File.swift
-//  
-//
 //  Created by Yannick Alexander on 28.05.20.
-//
 
 import Foundation
+
+extension Float {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Float.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension Float80 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Float80.self, data) else { return nil }
+        self = value
+    }
+}
