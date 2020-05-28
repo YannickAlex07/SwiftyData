ef8c4942760512b2185865c0aaa1eec640d0033d
Yannick Alexander <yannick.alexander07@gmail.com>
2020-05-28 15:41:02 +0200

    Updated Converter Tests
    The tests now use <Datatype>.self instead of wrong <Datatype>.Type.

--- Tests/SwiftyDataTests/ConverterTests.swift
+++ Tests/SwiftyDataTests/ConverterTests.swift
@@ -10,7 +10,7 @@ final class ConverterTests: XCTestCase {
             
         //Convert the sample data (8 zeroed bytes) into a Bool (1 Bit -> 1 Byte)
         //Should give back nil because the count of the sample data is too large for a bool
-        let convertedData = Converter.convertToType(Bool.Type, sampleData)
+        let convertedData = Converter.convertToType(Bool.self, sampleData)
         
         //Check that convertedData is Nil
         XCTAssertNil(convertedData)
@@ -21,7 +21,7 @@ final class ConverterTests: XCTestCase {
         
         //Try to convert the data (8 zeroed bytes) into an Int (64Bit -> 8 Byte)
         //Should give back an Int with the value of 0
-        let convertedData = Converter.convertToType(Int.Type, sampleData)
+        let convertedData = Converter.convertToType(Int.self, sampleData)
         
         //Check that convertedData is not Nil
         XCTAssertNotNil(convertedData)
