7a86f23998b8226cdc4d3b23663ae3bda6339715
Yannick Alexander <yannick.alexander07@gmail.com>
2020-05-28 16:06:12 +0200

    Added Int Inits
    The diffrent Integer Structs now implements Inits that allow the initialization from Data.

--- Sources/SwiftyData/Extensions/Datatypes/Int+Init.swift
+++ Sources/SwiftyData/Extensions/Datatypes/Int+Init.swift
@@ -1,8 +1,84 @@
-//
-//  File.swift
-//  
-//
 //  Created by Yannick Alexander on 28.05.20.
-//
 
 import Foundation
+
+extension Int {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Int.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension UInt {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(UInt.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension Int64 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Int64.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension UInt64 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(UInt64.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension Int32 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Int32.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension UInt32 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(UInt32.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension Int16 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Int16.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension UInt16 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(UInt16.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension Int8 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(Int8.self, data) else { return nil }
+        self = value
+    }
+}
+
+extension UInt8 {
+    init?(_ data: Data) {
+        //Convert Data and check that it return something else return nil
+        guard let value = Converter.convertToType(UInt8.self, data) else { return nil }
+        self = value
+    }
+}
+
