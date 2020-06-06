8693b0e78045af7e9df4ae8838f87a60d7296182
Yannick Alexander <yannick.alexander07@gmail.com>
2020-06-06 17:44:20 +0200

    Add Available Check For Float80
    Float80 is not available on iOS, therefore added an available check.

--- Sources/SwiftyData/Extensions/Datatypes/Float+Init.swift
+++ Sources/SwiftyData/Extensions/Datatypes/Float+Init.swift
@@ -10,6 +10,7 @@ public extension Float {
     }
 }
 
+@available(iOS 11, *)
 public extension Float80 {
     init?(_ data: Data) {
         //Convert Data and check that it return something else return nil
